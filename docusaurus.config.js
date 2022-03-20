eae8a3c74f75879d8cd4e26d3c9142fc636265ab
Dmytro Popov <dmytro.popov@era.io>
2022-03-20 15:42:37 +0200

    Add deploy config file;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "Peak Leap",
   tagline: "Dinosaurs are cool",
-  url: "https://dfpopov.github.io/dmytro-test.io/",
+  url: "https://dfpopov.github.io/",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -72,7 +72,6 @@ const config = {
           {
             type: "docsVersionDropdown",
             position: "right",
-            dropdownItemsAfter: [{ to: "/versions", label: "All versions" }],
             dropdownActiveClassDisabled: true,
           },
           {
---
+++ versioned_docs/version-1.1.0/documentation/credentials/issue-credential.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+id: issue-credential
+---
+
+# Issue Credential
